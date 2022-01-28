ed40960486fe81ca9869e260295f217031630ce8
brogala <brogala@sii.pl>
2022-01-28 09:46:04 +0100

    soft assertions added

--- src/test/java/fundamentals/FundamentalsServiceSpecification.java
+++ src/test/java/fundamentals/FundamentalsServiceSpecification.java
@@ -2,9 +2,6 @@ package fundamentals;
 
 public class FundamentalsServiceSpecification {
 
-//    spróbuj resources ogarnąć w testach
-
-
     public static String getSchema(String schemaName) {
         String schemaDirectory = "json/fundamentalsService/%s.json";
         return String.format(schemaDirectory, schemaName);
