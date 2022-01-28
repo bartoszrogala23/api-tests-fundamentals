7a3db310a1e0dcf85b67a9a20e087e70bde4bbaa
brogala <brogala@sii.pl>
2022-01-28 09:33:12 +0100

    soft assertions added

--- src/test/java/fundamentals/FundamentalsServiceTest.java
+++ src/test/java/fundamentals/FundamentalsServiceTest.java
@@ -1,5 +1,6 @@
 package fundamentals;
 
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,10 +13,20 @@ class FundamentalsServiceTest {
     @Test
     @DisplayName("check if 200 received")
     void getResponseOkTest() {
+        SoftAssertions softly = new SoftAssertions();
+
         var response = FundamentalsService.getResponseOk(SC_OK);
 
         response
                 .then()
                 .body(matchesJsonSchemaInClasspath(getSchema("getResponseOkSchema")));
+
+        softly.assertThat(response.body().asString())
+                .contains("This is GET example for status code 200");
+        softly.assertThat(response.body().asString())
+                .contains("OK");
+        softly.assertThat(response.body().asString())
+                .contains("The request has succeeded");
+        softly.assertAll();
     }
 }
