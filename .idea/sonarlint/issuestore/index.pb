7a24b8688b26256f1aaaf6451c15fa85591ae055
brogala <brogala@sii.pl>
2022-03-10 10:19:10 +0100

    add ServiceEndpoint, CryptoService

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,13 +7,5 @@ $.idea/codeStyles/codeStyleConfig.xml,d\d\dd7b3350c29c49497756c7f32c69a3bed18b1
 Nsrc/test/java/fundamentals/successfulRequestsResponses/getResponse200Test.java,3\7\37cc7cbf5ca8f78e59a184330d9719a2e673dc54
 ‡
 Wsrc/test/java/fundamentals/successfulRequestsResponses/getResponseOkWithParamsTest.java,a\9\a99fa6ffd5558014d93bfc1637aa0c4616ca9d14
-_
-/src/main/java/fundamentals/ServiceEndpoint.java,e\c\ec04809df774cdc01844e55aba5d4734851f9f26
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
-[
-+src/main/java/fundamentals/util/Values.java,2\1\21325355ca9ccb744594d18985c873fc983e0136
-c
-3src/main/java/fundamentals/FundamentalsService.java,c\8\c8e2ca214923b63eb77f3cc50342c9739c130f24
-Y
-)src/main/java/fundamentals/util/Util.java,5\3\53444c1bac45ff74b88c8fec52ab31b9c379ebc5
---
+++ src/main/java/crypto/ServiceEndpoint.java
@@ -0,0 +1,19 @@
+package crypto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ServiceEndpoint {
+    LOCAL_HOST("http://localhost:8084"),
+    GET_INFO(LOCAL_HOST.endpoint + "/challenge/acronym_agency/information"),
+    REGISTER(LOCAL_HOST.endpoint + "/challenge/acronym_agency/register"),
+    LOGIN(LOCAL_HOST.endpoint + "/challenge/acronym_agency/login"),
+    ENCRYPTED_MESSAGE(LOCAL_HOST.endpoint + "/challenge/acronym_agency/encrypted_message"),
+    GET_INFO_FROM_BOX(LOCAL_HOST.endpoint + "/challenge/acronym_agency/box"),
+    FINAL_MESSAGE_POST(LOCAL_HOST.endpoint + "/challenge/acronym_agency/{user_uuid}/headquarters"),
+    CRYPTO_ENGINE(LOCAL_HOST.endpoint + "/challenge/acronym_agency/{user_uuid}/crypto_engine");
+
+    private final String endpoint;
+}
