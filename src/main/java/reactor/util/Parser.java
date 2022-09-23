87a9c3b6ac37b4279155e32a4c9936895fa18625
bartosz <bartoszrogala23@gmail.com>
2022-09-23 10:07:57 +0200

    ControlRodTest added

--- src/main/java/reactor/util/Parser.java
+++ src/main/java/reactor/util/Parser.java
@@ -3,10 +3,8 @@ package reactor.util;
 import com.google.gson.Gson;
 import reactor.ReactorService;
 import reactor.model.CheckIn;
-import reactor.model.Reactor;
 
 import static org.apache.http.HttpStatus.SC_CREATED;
-import static org.apache.http.HttpStatus.SC_OK;
 
 public class Parser {
     private static final Gson gson = new Gson();
@@ -15,11 +13,5 @@ public class Parser {
         var response = ReactorService.postCheckIn(body, SC_CREATED);
 
         return gson.fromJson(response.body().asString(), CheckIn.class);
-    }
-
-    public static Reactor parseControlRoom(String key) {
-        var response = ReactorService.getControlRoom(key, SC_OK);
-
-        return gson.fromJson(response.body().asString(), Reactor.class);
     }
 }
