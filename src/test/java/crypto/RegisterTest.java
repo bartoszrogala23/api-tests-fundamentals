698d43bc2e49a4a6c7ddec74a2221342b3bf6eb7
bartosz <bartoszrogala23@gmail.com>
2022-09-23 12:46:54 +0200

    id getter method

--- src/test/java/crypto/RegisterTest.java
+++ src/test/java/crypto/RegisterTest.java
@@ -1,8 +1,10 @@
 package crypto;
 
+import crypto.model.User;
 import org.junit.jupiter.api.Test;
 
 import static crypto.util.Constants.REGISTER_MESSAGE;
+import static crypto.util.Util.*;
 import static groovy.json.JsonOutput.toJson;
 import static org.apache.http.HttpStatus.SC_CREATED;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -11,9 +13,14 @@ class RegisterTest extends CryptoBase{
 
     @Test
     void postRegisterTest() {
+        User user = createUser(faker.funnyName().name(), uniqueValue);
 
         var response = CryptoService.postRegister(toJson(user), SC_CREATED);
 
-        assertThat(response.getBody().asString()).contains(REGISTER_MESSAGE);
+        String responseText = response.getBody().asString();
+
+        assertThat(responseText).contains(REGISTER_MESSAGE);
+
+        String id = responseText.substring(responseText.lastIndexOf(" ") + 1, responseText.length() - 2);
     }
 }
